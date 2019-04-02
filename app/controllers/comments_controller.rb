c7a5a726580edb483482ef1e841489339ae1fb30
Aylin DeBruyne <adebruyne88@gmail.com>
2019-04-02 19:02:40 -0400

    deleting post being weird

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,10 @@ class CommentsController < ApplicationController
     def create
         @message =  Message.find(params[:message_id])
         @comment = @message.comments.create(comment_params)
-        @comment.user_id = current_user.user_id
+        @comment.user_id = current_user.id
 
         if @comment.save
-            redirect_to message_path(@messages)
+            redirect_to message_path(@message)
         else
             render 'new'
         end
